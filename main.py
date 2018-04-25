91e326caf062490bcf62a02069a40969fc791028
Nick Hahn <nick.hahn@hotmail.de>
2018-04-25 16:01:21 +0200

    Initial Push

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, session, redirect, url_for, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return "Logged in as %s" % escape(session['username'])
+    return "This is the landingpage"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username></p>
+            <p><input type=submit value=Login></p>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    session.pop("username", None)
+    return redirect(url_for('index'))
+
+@app.route('/u/<username>')
+def profile(username):
+    return "Userprofile of " + username
+
+app.secret_key = 'hello'
