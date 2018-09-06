2417b18c888e02f9792caae70c1d493238348ca5
Nick Hahn <nick.hahn@hotmail.de>
2018-09-07 00:23:41 +0200

    Start implementing database logic

--- app/models.py
+++ app/models.py
@@ -1,11 +1,17 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from flask_login import UserMixin
+from app import db, login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(256))
+
+    #name = db.Column(db.String(64))
+    #surname = db.Column(db.String(64))
+
+    
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -15,3 +21,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
