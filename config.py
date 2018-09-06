e8340ef1057e01dd05d7f8d205f13232642c8eef
Nick Hahn <nick.hahn@hotmail.de>
2018-09-07 00:24:16 +0200

    Fix typo in envar name => persistent database creation

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
 
 
-    SQL_ALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
