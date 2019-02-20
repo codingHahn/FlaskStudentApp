a8a63212c2dd418f8f67014a3f36f31fbb0a5bc4
Nick Hahn <nick.hahn@hotmail.de>
2019-02-20 16:59:33 +0100

    Add missing validators

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
-    username = StringField('Benutzername', validators=[DataRequired()])
-    password = StringField('Passwort', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Passwort', validators=[DataRequired()])
     remember_me = BooleanField('Angemeldet Bleiben')
     submit = SubmitField('Anmelden')
 
@@ -12,15 +12,16 @@ class EditProfileForm(FlaskForm):
     firstname = StringField('Vorname')
     surname = StringField('Nachname')
     text_area = TextAreaField('Über mich')
-    school_year = SelectField('Klasse' , choices=[(7, 7), (8, 8), (9, 9), (10, 10), (11, 11), (12, 12)])
+    school_year = SelectField('Klasse', choices=[(7, 7), (8, 8), (9, 9), (10, 10), (11, 11), (12, 12)])
     submit = SubmitField('Änderungen Übernehmen')
 
 class RegistrationForm(FlaskForm):
     firstname = StringField('Vorname', validators=[DataRequired()])
     surname = StringField('Nachname', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = StringField('Passwort', validators=[DataRequired()])   
-    school_year = SelectField('Klasse' , choices=[(7, 7), (8, 8), (9, 9), (10, 10), (11, 11), (12, 12)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Passwort', validators=[DataRequired()])
+    school_year = SelectField('Klasse',
+            choices=[(7, 7), (8, 8), (9, 9), (10, 10), (11, 11), (12, 12)], coerce=int)
     subject_latin = BooleanField('Latein')
     subject_math = BooleanField('Mathe')
     subject_english = BooleanField('Englisch')
